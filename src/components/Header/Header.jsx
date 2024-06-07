f539ad2bf6f422e477f58820062ad8c4e64251b7
KentChunga <kent.chungap@gmail.com>
2024-06-07 23:44:50 +0200

    remove menu buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Hamburger from "./Hamburger";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import Overlay from "./Overlay";
@@ -15,8 +14,8 @@ const Header = () => {
   return (
     <header className="relative mx-auto mb-4 mt-8 flex h-11 w-[90%] items-center justify-between md:w-[69.5rem] lg:w-[70%]">
       <Logo />
-      <Nav showMenu={isNavOpen} />
-      <Hamburger handleMenu={handleMenu} toggleMenu={isNavOpen} />
+      <Nav showMenu={isNavOpen} handleMenu={handleMenu} />
+      {/* <Hamburger handleMenu={handleMenu} toggleMenu={isNavOpen} /> */}
       <Overlay show={isNavOpen} />
     </header>
   );
